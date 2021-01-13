ab4d0bfbc6842e42a1bd5dacf986437154a7b2bd
Mari <mari325ilovecats@gmail.com>
2021-01-13 15:19:03 +0900

    Initial check-in of module WebAppHW

---
+++ WebAppHW/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppHW.Controllers
+{
+    public class HomeController:Controller
+    {
+        [HttpGet("/")]
+        public IActionResult Root()
+        {
+            return this.View();
+
+        }
+        [HttpGet("/Signup")]
+        public IActionResult Signup()
+        {
+            return this.View();
+        }
+    }
+}
